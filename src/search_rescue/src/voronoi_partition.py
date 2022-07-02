1b8f2148413fa0c8d300edf530bb5dc55064b903
jhan15 <jianminghan15@gmail.com>
2022-07-02 22:12:36 +0200

    use costmap

--- src/search_rescue/src/voronoi_partition.py
+++ src/search_rescue/src/voronoi_partition.py
@@ -24,6 +24,8 @@ class voronoi_partition:
         self.cm0 = None
         self.cm1 = None
         self.cm2 = None
+        self.exp_target = None
+        self.tb_goal = None
         self.target = None
         self.way_pt0 = PoseStamped()
         self.way_pt1 = PoseStamped()
@@ -60,95 +62,115 @@ class voronoi_partition:
         except KeyboardInterrupt:
           print("Shutting down")
 
-    # call back function for occupancy grid
+
     def og_callback(self, data):
+        """call back function for occupancy grid"""
         # rospy.loginfo("recieved data - occupancy grid")
         # the occupancy grid in a numpy array ->>>
         self.map = data
         self.og_to_numpy()
-        if self.target is None:
-            if self.count % self.freq == 0:
-                self.voronoi_compute()
-            self.count += 1
-        else:
+        if self.count % self.freq == 0:
             self.voronoi_compute()
+        self.count += 1
         
-    # call back function for robot1 position
+
     def tb0_callback(self, data):
+        """call back function for robot1 position"""
         # rospy.loginfo("recieved data - tb0")
         # print(data.pose.pose.position)
         self.tb0 = data
         # self.voronoi_compute()
     
-    # call back function for robot2 position
+
     def tb1_callback(self, data):
+        """call back function for robot2 position"""
         # rospy.loginfo("recieved data - tb1")
         self.tb1 = data
         # self.voronoi_compute()
         
-    # call back function for robot2 position
+
     def tb2_callback(self, data):
+        """call back function for robot2 position"""
         # rospy.loginfo("recieved data - tb2")
         self.tb2 = data
         # self.voronoi_compute()
     
-    # call back function for robot1 cost map
+
     def cm0_callback(self, data):
+        """call back function for robot1 cost map"""
         # rospy.loginfo("recieved data - cm0")
         self.cm0 = np.asarray(data.data, dtype=np.int8).reshape(data.info.height, data.info.width)
         # print('0-', self.cm0.shape)
     
-    # call back function for robot1 cost map
+
     def cm1_callback(self, data):
+        """call back function for robot1 cost map"""
         # rospy.loginfo("recieved data - cm1")
         self.cm1 = np.asarray(data.data, dtype=np.int8).reshape(data.info.height, data.info.width)
         # print('1-', self.cm1.shape)
     
-    # call back function for robot1 cost map
+
     def cm2_callback(self, data):
+        """call back function for robot1 cost map"""
         # rospy.loginfo("recieved data - cm2")
         self.cm2 = np.asarray(data.data, dtype=np.int8).reshape(data.info.height, data.info.width)
         # print('2-', self.cm2.shape)
     
-    # call back function for rescue target
+
     def target_callback(self, data):
+        """call back function for rescue target"""
         # rospy.loginfo("recieved data - target")
         self.target = data
     
-    # convert ocupancy grid to real world map coords
-    # input: the column and row number in the occupancy grid
-    # output: the x and y location on real map
+
     def og_to_world(self, value=0):
+        """
+        convert ocupancy grid to real world map coords
+        input: the column and row number in the occupancy grid
+        output: the x and y location on real map
+        """
         rows, columns = np.where(self.og_np==value)
         xs = round(self.map.info.origin.position.x,2) + round(self.map.info.resolution,2) * columns
         ys = round(self.map.info.origin.position.y,2) + round(self.map.info.resolution,2) * rows
         
-        return np.column_stack((xs, ys))
+        return np.column_stack((xs, ys)), np.column_stack((columns, rows))
 
-    # convert ocupancy grid to a numpy array
+
     def og_to_numpy(self):
+        """convert ocupancy grid to a numpy array"""
         data = np.asarray(self.map.data, dtype=np.int8).reshape(self.map.info.height,
                                                                 self.map.info.width)
         # data = np.ma.array(data, mask=data==-1, fill_value=-1)
         self.og_np = data
         # rospy.loginfo(self.og_np)
     
-    # check
+    def check_goal(self, robot_pos, th=1):
+        """check if one robot has reach the goal pose"""
+        for pos, goal_pos in zip(robot_pos, self.tb_goal):
+            if np.inner(pos-goal_pos, pos-goal_pos) < th:
+                return True
+        
+        return False
+    
+
     def info_available(self):
+        """chech required data is ready"""
         return self.tb0 is not None and self.tb1 is not None and \
             self.tb2 is not None and self.og_np is not None and \
             self.cm0 is not None and self.cm1 is not None and \
             self.cm2 is not None
         
-    # density function
+
     def density_function(self, points, target=None, cov=1):
+        """density function"""
         if target is not None:
             return multivariate_normal.pdf(points, mean=target, cov=cov)
         else:
             return np.ones(points.shape[0])
     
-    # voronoi tessellation
+
     def voronoi(self, generators, points, target, cov=1, n_iter=1):
+        """voronoi tessellation"""
         n_generator = generators.shape[0]
 
         for it in range(n_iter):
@@ -177,10 +199,10 @@ class voronoi_partition:
         
         return generators
 
-    # calculate voronoi partitions using occupancy grid and robot location
-    def voronoi_compute(self):
 
-        ##### insert voronoi code here #######
+    def voronoi_compute(self):
+        """calculate voronoi partitions using occupancy grid and robot location"""
+
         if self.info_available():
             # robot pos
             robots_pos = np.array([
@@ -190,31 +212,39 @@ class voronoi_partition:
             ])
 
             # free points
-            free_points = self.og_to_world()
+            free_points, free_points_grids = self.og_to_world()
 
             # unknown points
-            unknown_points = self.og_to_world(-1)
+            unknown_points, _ = self.og_to_world(-1)
 
-            # voronoi
-            target = unknown_points[np.random.randint(0,len(unknown_points))]
+            # voronoi compute
+            if self.tb_goal is not None and not self.check_goal(robots_pos):
+                target = self.exp_target
+            else:
+                target = unknown_points[np.random.randint(0,len(unknown_points))]
             new_robots_pos = self.voronoi(robots_pos, free_points, target, cov=1)
 
-            new_robots_pos[0] = free_points[np.argmin([np.inner(new_robots_pos[0]-point, new_robots_pos[0]-point) \
-                for point in free_points])]
-            new_robots_pos[1] = free_points[np.argmin([np.inner(new_robots_pos[1]-point, new_robots_pos[1]-point) \
-                for point in free_points])]
-            new_robots_pos[2] = free_points[np.argmin([np.inner(new_robots_pos[2]-point, new_robots_pos[2]-point) \
-                for point in free_points])]
+            self.exp_target = target
+            self.tb_goal = np.copy(new_robots_pos)
 
-            # cost
-            # new_robots_pos[0] = free_points[np.argmin([np.inner(new_robots_pos[0]-point, new_robots_pos[0]-point) \
-            #     for point, grid in zip(free_points, free_points_grids) if self.cm0[grid[1],grid[0]]<99])]
-            
-            # new_robots_pos[1] = free_points[np.argmin([np.inner(new_robots_pos[1]-point, new_robots_pos[1]-point) \
-            #     for point, grid in zip(free_points, free_points_grids) if self.cm1[grid[1],grid[0]]<99])]
-            
-            # new_robots_pos[2] = free_points[np.argmin([np.inner(new_robots_pos[2]-point, new_robots_pos[2]-point) \
-            #     for point, grid in zip(free_points, free_points_grids) if self.cm2[grid[1],grid[0]]<99])]
+            # adjust goal pos by costmap
+            dist0 = np.array([np.inner(new_robots_pos[0]-point, new_robots_pos[0]-point) for point in free_points])
+            cost0 = np.array([self.cm0[grid[1],grid[0]] for grid in free_points_grids])
+            free_points0 = free_points[cost0 < 20]
+            dist0 = dist0[cost0 < 20]
+            new_robots_pos[0] = free_points0[np.argmin(dist0)]
+
+            dist1 = np.array([np.inner(new_robots_pos[1]-point, new_robots_pos[1]-point) for point in free_points])
+            cost1 = np.array([self.cm1[grid[1],grid[0]] for grid in free_points_grids])
+            free_points1 = free_points[cost1 < 20]
+            dist1 = dist1[cost1 < 20]
+            new_robots_pos[1] = free_points1[np.argmin(dist1)]
+
+            dist2 = np.array([np.inner(new_robots_pos[2]-point, new_robots_pos[2]-point) for point in free_points])
+            cost2 = np.array([self.cm2[grid[1],grid[0]] for grid in free_points_grids])
+            free_points2 = free_points[cost2 < 20]
+            dist2 = dist2[cost2 < 20]
+            new_robots_pos[2] = free_points2[np.argmin(dist2)]
 
             print('----------------------- voronoi -------------------------')
             print('target:', target)
