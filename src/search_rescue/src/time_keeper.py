0175ad6d9c1d6d5d8831be6d5dfed98a97f63b72
Basem Shaker <bshaker@kth.se>
2022-07-08 15:02:49 +0200

    finished testing results

--- src/search_rescue/src/time_keeper.py
+++ src/search_rescue/src/time_keeper.py
@@ -18,7 +18,8 @@ class time_keeper:
         self.counter0 = 0  
         self.counter1 = 0  
         self.counter2 = 0
-
+        self.row_range = [45, 350]
+        self.col_range = [25, 371]
         rospy.Subscriber("/target_pose", PointStamped, self.tp_callback)
         rospy.Subscriber("tb3_0/move_base_simple/goal", PoseStamped, self.mbg0_callback)
         rospy.Subscriber("tb3_1/move_base_simple/goal", PoseStamped, self.mbg1_callback)
@@ -36,6 +37,7 @@ class time_keeper:
         data = np.asarray(self.map.data, dtype=np.int8).reshape(self.map.info.height,
                                                                 self.map.info.width)
         self.og_np = data
+        self.og_np = self.og_np[45:350,25:371]
 
     def mbg0_callback(self, data):
         if self.counter0 == 0:
@@ -78,13 +80,13 @@ class time_keeper:
         self.percent_discovered = 1.0 - (float(self.not_discovered.size) / float(self.og_np.size))
         print("percent:" ,self.percent_discovered)
         # List 
-        List=[self.robot_count,self.time_elapsed,self.counter0,self.counter1,self.counter2,self.percent_discovered]
+        List=[self.robot_count,self.time_elapsed,self.counter0,self.counter1,self.counter2,self.percent_discovered,'y']
         
         with open(r'time_keeper.csv', 'a', newline='') as csvfile:
-            fieldnames = ['robot_count','time_taken','waypoint_count_0','waypoint_count_1','waypoint_count_2','perc_disc']
+            fieldnames = ['robot_count','time_taken','waypoint_count_0','waypoint_count_1','waypoint_count_2','perc_disc','new']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            writer.writerow({'robot_count':List[0], 'time_taken':List[1],'waypoint_count_0':List[2],'waypoint_count_1':List[3],'waypoint_count_2':List[4],'perc_disc':List[5]})
+            writer.writerow({'robot_count':List[0], 'time_taken':List[1],'waypoint_count_0':List[2],'waypoint_count_1':List[3],'waypoint_count_2':List[4],'perc_disc':List[5],'new':List[6]})
 
         rospy.signal_shutdown("shutting down")
 
